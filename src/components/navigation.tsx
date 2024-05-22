6639d899387f0c5a58402d308dcd04fa0675e293
vmnog <vmnogueira.ti@gmail.com>
2024-05-22 19:33:05 -0300

    style: change white color to primary usage in navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 export function Navigation() {
   return (
     <nav className='flex items-center gap-4'>
-      <Link href="/" className="hover:border-b-white hover:border-b">
+      <Link href="/" className="hover:border-b-primary hover:border-b">
         Home
       </Link>
-      <Link href="/about" className="hover:border-b-white hover:border-b">
+      <Link href="/about" className="hover:border-b-primary hover:border-b">
         About
       </Link>
-      <Link href="/blog" className="hover:border-b-white hover:border-b">
+      <Link href="/blog" className="hover:border-b-primary hover:border-b">
         Blog
       </Link>
     </nav>
